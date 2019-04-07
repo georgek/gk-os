d0a2b80fb4c955c30f94650f673b1703135dbe5f
George Kettleborough <kettleg@gmail.com>
2019-04-07 17:52:41 +0100

    Add 0x to hex

--- kprintf.c
+++ kprintf.c
@@ -10,6 +10,9 @@ void kprinthex(unsigned int d)
 {
      unsigned char c;
      int i;
+
+     kputc('0');
+     kputc('x');
      for (i = 28; i >= 0; i -= 4) {
           c = (d >> i) & 0xF;
           c += c > 9 ? 0x37 : 0x30;
