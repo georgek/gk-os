a4b107fdd17309108b5750fc55ce48b717b2a3f6
George Kettleborough <kettleg@gmail.com>
2019-04-13 18:58:39 +0100

    Boot into EL1

--- start.s
+++ start.s
@@ -14,6 +14,44 @@ _start:
         b       1b
 
 2:      ldr     x1, =_start
+
+        // get into EL1
+        mrs     x0, CurrentEL
+        and     x0, x0, #12     // clear reserved bits
+
+        // check if running at EL3
+        cmp     x0, #12
+        bne     el2
+        // in EL3
+        mov     x2, #0x5b1
+        msr     scr_el3, x2
+        mov     x2, #0x3c9
+        msr     spsr_el3, x2
+        adr     x2, el2
+        msr     elr_el3, x2
+        eret
+
+        // check if running at EL2
+el2:    cmp     x0, #4
+        beq     set_stack
+        msr     sp_el1, x1
+        // enable cntp for EL1
+        mrs     x0, cnthctl_el2
+        orr     x0, x0, #3
+        msr     cnthctl_el2, x0
+        msr     cntvoff_el2, xzr
+        // enable aarch64 in EL1
+        mov     x0, #(1 << 31)  // aarch64
+        orr     x0, x0, #(1 << 1) // SWIO hardwired on Pi3
+        msr     hcr_el2, x0
+        // change to EL1
+        mov     x2, #0x3c4
+        msr     spsr_el2, x2
+        adr     x2, set_stack
+        msr     elr_el2, x2
+        eret
+
+set_stack:
         mov     sp, x1
 
         ldr     x1, =__bss_start
